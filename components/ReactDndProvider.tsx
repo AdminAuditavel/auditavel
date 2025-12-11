f19efb3505f82545d271464484252ea12d1ebb23
AdminAuditavel <admin@auditavel.com>
2025-12-11 09:15:00 -0300

    Update ReactDndProvider.tsx

--- components/ReactDndProvider.tsx
+++ components/ReactDndProvider.tsx
@@ -4,5 +4,6 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export default function ReactDndProvider({ children }: { children: React.ReactNode }) {
+  console.log('[ReactDndProvider] mounted'); // <-- passo único: verifica se o provider existe no DOM
   return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
 }
