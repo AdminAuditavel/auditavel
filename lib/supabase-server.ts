c4aadfa7884bc44d020f82d927a68c39377c87ee
AdminAuditavel <admin@auditavel.com>
2026-01-05 22:35:23 -0300

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,14 +1,25 @@
 //lib/supabase-server.ts
 
-import { createServerClient } from '@supabase/auth-helpers-nextjs'; // Usando a função correta
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export const supabaseServer = () => {
-  console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL); // Verifique se a URL está correta
-  console.log('Supabase Anon Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY); // Verifique se a chave está correta
+export function supabaseServer() {
+  const cookieStore = cookies();
 
-  // Retornando o cliente Supabase configurado corretamente
-  return createServerClient({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  });
-};
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+}
